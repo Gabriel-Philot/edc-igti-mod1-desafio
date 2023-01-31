8c4744a203b5d69cce67853511f9d4886c19e077
Gabriel-Philot <bilphilot@gmail.com>
2023-01-30 21:23:32 -0300

    Add infra(glue job & crawler) & etl script

--- infra_terraform/s3_files.tf
+++ infra_terraform/s3_files.tf
@@ -1,16 +1,7 @@
-# criando branch dev
-## File spark job for emr
-# resource "aws_s3_object" "codigo_spark_emr" {
-#   bucket = aws_s3_bucket.datalake.id #Referêrncia do datalake criado
-#   key    = "emr-code/pyspark/emr_job_spark_fro_tf.py"
-#   source = "../job_spark.py"          # Arquivo com o job spark que vai
-#   etag   = filemd5("../job_spark.py") # Define qual é o objeto de parâmetro
-# }
 
-# File spark job for glue
-# resource "aws_s3_object" "codigo_spark_glue" {
-#   bucket = aws_s3_bucket.datalake.id #Referêrncia do datalake criado
-#   key    = "glue_job-code/pyspark/gluejob_spark_fro_tf.py"
-#   source = "../job_spark_glue.py"          # Arquivo com o job spark que vai
-#   etag   = filemd5("../job_spark_glue.py") # Define qual é o objeto de parâmetro
-# }
+resource "aws_s3_object" "codigo-glue-job" {
+  bucket = "${var.base_bucket_name}-${var.ambiente}"
+  key    = "/scripts/glue_job_pyspark.py" # Diretório onde será gravado o arquivo
+  source = "../etl/glue_job_pyspark.py"          # Arquivo com o job que vai subir
+  etag   = filemd5("../etl/glue_job_pyspark.py") # Define qual é o objeto de parâmetro
+} 
--- infra_terraform/variables.tf
+++ infra_terraform/variables.tf
@@ -5,3 +5,19 @@ variable "base_bucket_name" {
 variable "ambiente" {
   default = "production"
 }
+
+variable "glue_job_name" {
+  default = "glue_job_spark_rais_challenge"
+}
+
+variable "glue_crawler_name" {
+  default = "glue_crawler_rais_challenge"
+}
+# URI da IAM Role criada no AWS.
+variable "iam_arn" {
+  default = "arn:aws:iam::415658869338:role/AWSGlueServiceRole-admin"
+}
+
+variable "database_name" {
+  default = "database-rais-challenge"
+}
